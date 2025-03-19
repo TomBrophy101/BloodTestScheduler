3ce481abbbd333d9a9bcb6ecc30d8bdf49ea7da6
TomBrophy101 <tombrophy101@outlook.com>
2025-03-19 18:56:02 +0000

    Updated files

--- src/bloodtestscheduler/BloodTestSchedulerApp.java
+++ src/bloodtestscheduler/BloodTestSchedulerApp.java
@@ -23,8 +23,6 @@ public class BloodTestSchedulerApp {
     
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        
         BloodTestSchedulerGUI myGUI = new BloodTestSchedulerGUI();
         myGUI.setVisible(true);
     }
