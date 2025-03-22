0b5e009b0e1da167f74ff6ee8b98e0701aa870d2
TomBrophy101 <tombrophy101@outlook.com>
2025-03-22 09:11:06 +0000

    updated files

--- src/bloodtestscheduler/BloodTestSchedulerApp.java
+++ src/bloodtestscheduler/BloodTestSchedulerApp.java
@@ -14,14 +14,21 @@ public class BloodTestSchedulerApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Patient temp1 = new Patient("Tom", 23, "Dr Holmes", 2, true);
-        Patient temp2 = new Patient("Jordan", 26, "Dr Watson", 1, false);
+        Patient temp1 = new Patient("Tom Brophy", 23, "Dr Holmes", 2, true);
+        Patient temp2 = new Patient("Jordan Carthy", 26, "Dr Watson", 1, false);
+        Patient temp3 = new Patient("Tre Roberts", 22, "Dr Hibbert", 0, false);
+        Patient temp4 = new Patient("Roko Makanovic", 30, "Dr Nick", 1, true);
+        Patient temp5 = new Patient("CJ Smith", 24, "Dr Doom", 2, false);
         NoShowList missedPatients = new NoShowList();
         missedPatients.push(temp1);
         missedPatients.push(temp2);
+        missedPatients.push(temp3);
+        missedPatients.push(temp4);
+        missedPatients.push(temp5);
+        
         
         MyBloodTestScheduler queuePatients = new MyBloodTestScheduler();
-        queuePatients.enqueue("Nisa", 21, "Dr House", 0, true);
+        queuePatients.enqueue("Nisa Agayev", 21, "Dr House", 0, true);
         
         
         
